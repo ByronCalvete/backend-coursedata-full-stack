e7b6028313ef680760ff0fb8b8ce939efb2f1600
Byron Calvete <calvete94@gmail.com>
2023-04-24 19:03:55 -0300

    add: errorhandling and connect with MongoDB put and delete request

--- index.js
+++ index.js
@@ -4,28 +4,9 @@ const Note = require('./models/note')
 
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
-
-// This not necessary because this data come from the MongoDB
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     important: true
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only JavaScript",
-//     important: false
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true
-//   }
-// ]
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -47,19 +28,16 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  // const id = Number(request.params.id)
-  // const note = notes.find(note => note.id === id)
-
-  // if(note) {
-  //   response.json(note)
-  // } else {
-  //   response.statusMessage = `There are no resources corresponding to the note with id number ${id}`
-  //   response.status(404).end()
-  // }
-  Note.findById(request.params.id).then(note =>  {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note =>  {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -76,17 +54,34 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false
   })
 
-  // notes = [...notes, note]
   note.save().then(savedNote => {
     console.log('note saved!')
     response.json(savedNote)
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      // For consistency the status code should be the same independient if the resource exist or doesn't
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -95,16 +90,19 @@ const unknownEndpoint = (request, response) => {
   })
 }
 
-app.put('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  const updatedNote = {...note, important: !note.important}
-
-  notes = notes.map(note => note.id === id ? updatedNote : note)
-  response.json(updatedNote)
-})
-
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
