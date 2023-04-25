93d82f54219d916694df3e691372d8a82f07c150
Byron Calvete <calvete94@gmail.com>
2023-04-25 08:12:12 -0300

    add: validations in post and put methods

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if(!body.content) {
@@ -54,10 +54,12 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false
   })
 
-  note.save().then(savedNote => {
-    console.log('note saved!')
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      console.log('note saved!')
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -77,7 +79,7 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -97,6 +99,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
 
   next(error)
--- models/note.js
+++ models/note.js
@@ -13,7 +13,11 @@ mongoose.connect(process.env.MONGODB_CONNECTION)
   })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
   important: Boolean,
 })
 
